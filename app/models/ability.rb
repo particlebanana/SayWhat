3cc1e65d30e65ec5165a5ca4bd13d2ebea7e0032
particlebanana <particlebanana@gmail.com>
2010-11-03 16:51:47 -0500

    cancan permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,13 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(current_user)
     
+    if !current_user
+      can :request_group, Group
+      can :create_request, Group
+      can :pending_request, Group
+    end
+      
   end
 end
