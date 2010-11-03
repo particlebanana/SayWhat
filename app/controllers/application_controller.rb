a5828183e38c39abb179af02b51053a8f995de07
particlebanana <particlebanana@gmail.com>
2010-11-03 16:55:49 -0500

    cancan error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  class ApplicationController < ActionController::Base
+    rescue_from CanCan::AccessDenied do |exception|
+      flash[:alert] = exception.message
+      redirect_to root_url
+    end
+  end
+  
 end
