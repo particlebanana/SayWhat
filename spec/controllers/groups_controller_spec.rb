5f763e213fd0c4c62a486bffe087e2b20b631cb0
particlebanana <particlebanana@gmail.com>
2010-11-04 17:16:09 -0500

    approve a group

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GroupsController do
   
   describe "#request_group" do
-    request_attributes = {
+    request = {
       :group => {
         :name => "Han Shot First",
         :city => "Mos Eisley",
@@ -20,9 +20,30 @@ describe GroupsController do
     it "should add a pending group" do  
       lambda {
         lambda {
-          post :create_request, request_attributes
+          post :create, request
         }.should change(Group, "count").by(1)
       }.should change(User, "count").by(1)
+    end
+    
+  end
+    
+  describe "#pending_group" do
+    
+    before do
+      @group = Factory.create(:pending_group)
+      login_admin
+    end
+    
+    it "should change a group's status to active" do
+      request = {
+        :name => "Han Shot First",
+        :city => "Mos Eisley",
+        :organization => "Free Greedo"
+      }
+      
+      put :approve_group, {:id => @group.id.to_s, :group => request}
+      @group = Group.find(@group.id.to_s)
+      @group.status.should == "active"
     end
     
   end
