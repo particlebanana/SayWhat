cdb16d586e968689c023e415e2afbe1b06214c6f
particlebanana <particlebanana@gmail.com>
2010-11-05 15:29:16 -0500

    added ability to login with token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,23 @@
 class User
   include Mongoid::Document      
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :token_authenticatable, :database_authenticatable, :recoverable, :rememberable, :validatable
   field :username
   field :first_name
   field :last_name
   field :role
+  field :status
+  field :authentication_token
   referenced_in :group
 
   attr_accessible :email, :username, :first_name, :last_name, :password, :password_confirmation, :remember_me
 
-  validates_presence_of [:first_name, :last_name, :email, :username, :role]
+  validates_presence_of [:first_name, :last_name, :email, :username, :role, :status]
   validates_uniqueness_of [:username]
   
   before_validation :downcase_attributes
   before_validation :generate_temp_password
+  
+  before_create :generate_token
   
   protected
   
@@ -23,10 +27,14 @@ class User
     end
   
     def generate_temp_password
-      chars = ("a".."z").to_a + ("1".."9").to_a 
-      tempPass = Array.new(8, '').collect{chars[rand(chars.size)]}.join
-      self.password ||= tempPass
-      self.password_confirmation ||= tempPass
+      temp_pass = generate_random_key
+      self.password ||= temp_pass
+      self.password_confirmation ||= temp_pass
+    end
+    
+    def generate_token
+      key = generate_random_key
+      self.authentication_token = key
     end
 
     
@@ -36,7 +44,12 @@ class User
       role == "admin" ? true : false
     end
 
-  #private
+  private
+  
+    def generate_random_key
+      chars = ("a".."z").to_a + ("1".."9").to_a 
+      key = Array.new(8, '').collect{chars[rand(chars.size)]}.join
+    end
   
   #  def active?
   #    super && account_active?
