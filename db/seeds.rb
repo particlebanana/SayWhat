7b812596c62f47271d21f4361b944d4b14798cd0
particlebanana <particlebanana@gmail.com>
2010-11-11 16:20:29 -0600

    edit db seed for easier email testing locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name == 'system.indexes'}.each(&:drop)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.new(:first_name => 'administrator', :last_name => "Of The World", :email => 'particlebanana@gmail.com', :password => 'admin123', :password_confirmation => 'admin123')
+user = User.new(:first_name => 'Ruler', :last_name => "Of The World", :email => 'cs62@txstate.edu', :password => 'admin123', :password_confirmation => 'admin123')
 user.role = 'admin'
 user.status = 'active'
 user.save!
-puts 'New user created: ' << user.username
+puts 'New user created: ' << user.email
 puts 'Password is admin123'
