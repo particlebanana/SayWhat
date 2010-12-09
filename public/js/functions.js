eb852c7dfc7af23767270f8f8dfa9c8d7d9caed4
particlebanana <particlebanana@gmail.com>
2010-12-09 09:39:49 -0600

    add placeholder image for project index

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){	
   
-  // Manage display state for User avatar edit options
-  if ( $('.avatarOptions').length > 0 ) {
-    $('.avatarUpload').hide();
-    $('#changeAvatar').click(function(){
-      $('.avatarOptions').hide();
-      $('.avatarUpload').show();
-      return false;
-    })
-  } else {
-    $('.avatarUpload').show();
-  }
+	// All Projects :nth-child selector (for use in IE)
+	$("#projectsList li:nth-child(4n+1)").addClass("nthLeft");
+	$('#projectsList li:nth-child(4n+4)').addClass("nthRight");
+
+	// Manage display state for User avatar edit options
+	if ( $('.avatarOptions').length > 0 ) {
+		$('.avatarUpload').hide();
+		$('#changeAvatar').click(function(){
+			$('.avatarOptions').hide();
+			$('.avatarUpload').show();
+			return false;
+		})
+	} else {
+		$('.avatarUpload').show();
+	}
 
 });
