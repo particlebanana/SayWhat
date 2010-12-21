b147d67336a81f4242d1fbf53c7ca67219ef0bc9
particlebanana <particlebanana@gmail.com>
2010-12-21 10:43:55 -0600

    fix themes path

--- config/initializers/load_theme_config.rb
+++ config/initializers/load_theme_config.rb
@@ -2,5 +2,5 @@ raw_config = File.read(Rails.root.to_s + "/config/theme.yml")
 THEME = YAML.load(raw_config)
 
 Haml::Template::options[:format] = :html5
-Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + THEME['theme'] + THEME['stylesheets']).to_s
+Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + 'themes' + THEME['theme'] + THEME['stylesheets']).to_s
 Sass::Plugin.options[:css_location]       = (Rails.root + 'public' + 'stylesheets').to_s
