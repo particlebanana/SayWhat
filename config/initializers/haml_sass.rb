0e84210001d0c9a4fb9aed12a0e2ae3fa92274fc
particlebanana <particlebanana@gmail.com>
2010-12-21 09:09:41 -0600

    load theme from directory specified in config/theme.yml along with an example file

--- config/initializers/haml_sass.rb
+++
@@ -1,4 +0,0 @@
-Haml::Template::options[:format] = :html5
-
-Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + 'styles').to_s
-Sass::Plugin.options[:css_location]       = (Rails.root + 'public' + 'stylesheets').to_s
---
+++ config/initializers/load_theme_config.rb
@@ -0,0 +1,6 @@
+raw_config = File.read(Rails.root.to_s + "/config/theme.yml")
+THEME = YAML.load(raw_config)
+
+Haml::Template::options[:format] = :html5
+Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + THEME['theme'] + THEME['stylesheets']).to_s
+Sass::Plugin.options[:css_location]       = (Rails.root + 'public' + 'stylesheets').to_s
