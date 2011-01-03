8a29573838fcd321d8bd00ffacf0bc29bbf898c7
particlebanana <particlebanana@gmail.com>
2011-01-03 13:43:54 -0600

    jquery nth child selectors

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){	
+head.ready(function() {
   
 	// All Projects :nth-child selector (for use in IE)
-	$("#projectsList li:nth-child(4n+1)").addClass("nthLeft");
-	$('#projectsList li:nth-child(4n+4)').addClass("nthRight");
+	$(".projectsList li:nth-child(4n+1)").addClass("nthLeft");
+	$('.projectsList li:nth-child(4n+4)').addClass("nthRight");
 
 	// Manage display state for User avatar edit options
 	if ( $('.avatarOptions').length > 0 ) {
