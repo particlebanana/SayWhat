df37df3b96aac97292eb359af5dbdd0ecbc0923a
particlebanana <particlebanana@gmail.com>
2011-01-25 12:16:55 -0600

    IE css styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,20 @@ module ApplicationHelper
     #str.gsub(/^(.{10}[\w.]*)(.*)/) {$2.empty? ? $1 : $1 + '…'}
     str.split[0..(limit-1)].join(" ") +(str.split.size > limit ? "..." : "") 
   end
+  
+  # Implements the Paul Irish IE conditional comments HTML tag--in HAML.
+  # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+  def cc_html(options={}, &blk)
+    attrs = options.map { |(k, v)| " #{h k}='#{h v}'" }.join('')
+    [ "<!--[if lt IE 7 ]> <html#{attrs} class='ie6'> <![endif]-->",
+      "<!--[if IE 7 ]>    <html#{attrs} class='ie7'> <![endif]-->",
+      "<!--[if IE 8 ]>    <html#{attrs} class='ie8'> <![endif]-->",
+      "<!--[if IE 9 ]>    <html#{attrs} class='ie9'> <![endif]-->",
+      "<!--[if (gt IE 9)|!(IE)]><!--> <html#{attrs}> <!--<![endif]-->",
+      capture_haml(&blk).strip,
+      "</html>"
+    ].join("\n")
+  end
 
 
 end
