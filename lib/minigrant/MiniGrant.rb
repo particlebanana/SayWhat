8f6cff5a8b970d6e75646a40cec6dd697bb19a1d
particlebanana <particlebanana@gmail.com>
2011-01-31 11:15:45 -0600

    heroku sendgrid changes

--- lib/minigrant/MiniGrant.rb
+++ lib/minigrant/MiniGrant.rb
@@ -30,12 +30,30 @@ module MiniGrant
         Pony.mail :to => params['application']['adult_email'],
                   :from => "admin@txsaywhat.com",
                   :subject => "SayWhat! Mini-Grant Information",
-                  :body => erb(:approved_email)
+                  :body => erb(:approved_email),
+                  :via => :smtp,
+                  :smtp => {
+                    :port   => '25',
+                    :user_name  => ENV['SENDGRID_USERNAME'],
+                    :password   => ENV['SENDGRID_PASSWORD'],
+                    :address   => 'smtp.sendgrid.net',
+                    :authentication   => :plain,
+                    :domain => ENV['SENDGRID_DOMAIN']
+                  }
                   
         Pony.main :to => "info@txsaywhat.com",
                   :from => "admin@txsaywhat.com",
                   :subject => "SayWhat! Mini-Grant Application",
-                  :body => erb(:application_email)
+                  :body => erb(:application_email),
+                  :via => :smtp,
+                  :smtp => {
+                    :port   => '25',
+                    :user_name  => ENV['SENDGRID_USERNAME'],
+                    :password   => ENV['SENDGRID_PASSWORD'],
+                    :address   => 'smtp.sendgrid.net',
+                    :authentication   => :plain,
+                    :domain => ENV['SENDGRID_DOMAIN']
+                  }
                             
         redirect '/grants/completed'
       else
