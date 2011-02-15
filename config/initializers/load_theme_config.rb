17bf96077b8cd1c769a07bb830b45ce366bd55fe
particlebanana <particlebanana@gmail.com>
2011-02-15 10:11:24 -0600

    compress stylesheets

--- config/initializers/load_theme_config.rb
+++ config/initializers/load_theme_config.rb
@@ -6,6 +6,7 @@ THEME = YAML.load(raw_config)
 Haml::Template::options[:format] = :html5
 Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + 'themes' + THEME['theme'] + THEME['stylesheets']).to_s
 Sass::Plugin.options[:css_location]       = (Rails.root + 'public' + 'stylesheets').to_s
+Sass::Plugin.options[:style] = :compressed
 
 class ServeStaticThemeFiles
   
