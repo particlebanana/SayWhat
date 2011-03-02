7c322dba5300eb5b1be0ed68b8580657ee0e41ed
Cody Stoltman <particlebanana@gmail.com>
2011-03-02 09:45:17 -0600

    fix db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name == 'system.indexes'}.each(&:drop)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.new(:first_name => 'Ruler', :last_name => "Of The World", :email => 'cs62@txstate.edu', :password => 'admin123', :password_confirmation => 'admin123')
+user = User.new(:first_name => 'Ruler', :last_name => "Of The World", :email => 'admin@test.com', :password => 'admin123', :password_confirmation => 'admin123')
 user.role = 'admin'
 user.status = 'active'
 user.save!
