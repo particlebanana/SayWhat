613f209684053ac00bf8ba7fb38d163ff663ea1b
Cody Stoltman <particlebanana@gmail.com>
2011-05-31 09:21:09 -0500

    only show active groups in dashboard count

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,7 @@ class AdminController < ApplicationController
   def index
     @group_requests = Group.where(:status => "pending").count
     @grant_requests = Grant.pending.count
-    @groups = Group.count
+    @groups = Group.where(:status => "active").count
     @projects = ProjectCache.count
   end
   
