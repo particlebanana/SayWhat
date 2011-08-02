43705dd6c67e14cd6b79ff88a8011f459aee4e63
Cody Stoltman <particlebanana@gmail.com>
2011-08-02 14:36:53 -0500

    request membership form design changes

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require File.expand_path('../application', __FILE__)
 SayWhat::Application.initialize!
 
 Haml::Template.options[:format] = :html5
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  '<span class="field_with_errors">'.html_safe << html_tag << 
+'</span>'.html_safe
+end
