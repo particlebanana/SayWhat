8f3baba61a08f9d6643469416a47004ea09ac4ce
Cody Stoltman <particlebanana@gmail.com>
2011-08-17 11:18:26 -0500

    clean the sql test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'carrierwave/test/matchers'
+require 'database_cleaner'
     
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -12,8 +13,18 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
-
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+    
   config.before :each do
+    DatabaseCleaner.start
     Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
   end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end  
 end
