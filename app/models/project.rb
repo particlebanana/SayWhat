723279934f6f185922cbf0ed4d14c949e10081ce
Cody Stoltman <particlebanana@gmail.com>
2011-08-31 16:30:59 -0500

    refactor project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,34 +8,29 @@ class Project < ActiveRecord::Base
   attr_protected :name, :group, :comments
 
   validates_presence_of [:name, :display_name, :location, :start_date, :end_date, :focus, :goal, :description, :audience, :involves]
-  validates_uniqueness_of [:name]
+  validates_uniqueness_of :name
   
   before_validation :escape_name
   after_validation :sanitize
-
+  
+  def filters
+    focus = ['Filter by Focus', 'Secondhand Smoke Exposure', 'General Education', 'Health Effects', 'Policy Focused', 'Industry Manipulation', 'Access/Enforcement', 'Marketing/Advertising']
+    audience = ['Filter by Audience', 'Elementary Students', 'Middle School Students', 'High School Students', 'Community Members', 'Government Officials']
+    filters = {
+      focus: focus.map { |focus| [focus, focus] },
+      audience: audience.map { |audience| [audience, audience] }
+    }
+  end
+  
   protected
-  
-    def escape_name
-      if self.display_name
-        self.name = (self.display_name.downcase.gsub(/[^a-zA-Z 0-9]/, "")).gsub(/\s/,'-')
-      end
-    end
-    
-    def sanitize
-      self.goal = Sanitize.clean(self.goal, Sanitize::Config::RESTRICTED) if self.goal
-      self.description = Sanitize.clean(self.description, Sanitize::Config::RESTRICTED) if self.description
-      self.involves = Sanitize.clean(self.involves, Sanitize::Config::RESTRICTED) if self.involves
-    end
-        
-  public
-  
-    def filters
-      focus = ['Filter by Focus', 'Secondhand Smoke Exposure', 'General Education', 'Health Effects', 'Policy Focused', 'Industry Manipulation', 'Access/Enforcement', 'Marketing/Advertising']
-      audience = ['Filter by Audience', 'Elementary Students', 'Middle School Students', 'High School Students', 'Community Members', 'Government Officials']
-      filters = {
-        :focus => focus.map { |focus| [focus, focus] },
-        :audience => audience.map { |audience| [audience, audience] }
-      }
-    end
-        
+
+  def escape_name
+    self.name = (self.display_name.downcase.gsub(/[^a-zA-Z 0-9]/, "")).gsub(/\s/,'-') if self.display_name
+  end
+
+  def sanitize
+    self.goal = Sanitize.clean(self.goal, Sanitize::Config::RESTRICTED) if self.goal
+    self.description = Sanitize.clean(self.description, Sanitize::Config::RESTRICTED) if self.description
+    self.involves = Sanitize.clean(self.involves, Sanitize::Config::RESTRICTED) if self.involves
+  end      
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,78 +1,37 @@
 require 'spec_helper'
 
 describe Project do
-  
-  describe "validations" do
-    describe "of required fields" do 
-      it "should fail if display name is blank" do
-        @project = Factory.build(:project, :display_name => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if location is blank" do
-        @project = Factory.build(:project, :location => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if start date is blank" do
-        @project = Factory.build(:project, :start_date => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if end date is blank" do
-        @project = Factory.build(:project, :end_date => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if focus is blank" do
-        @project = Factory.build(:project, :focus => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if goal is blank" do
-        @project = Factory.build(:project, :goal => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if description is blank" do
-        @project = Factory.build(:project, :description => nil)
-        @project.should_not be_valid
-      end 
-      
-      it "should fail if audience is blank" do
-        @project = Factory.build(:project, :audience => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should fail if involves is blank" do
-        @project = Factory.build(:project, :involves => nil)
-        @project.should_not be_valid
-      end
-      
-      it "should validate uniqueness of name" do
-        project = Factory.build(:project)
-        project.save
-        project2 = Factory.build(:project)
-        project2.should_not be_valid
-        project2.display_name = 'project 2'
-        project2.should be_valid
-      end
-      
-    end
+  context "Factory" do
+    before { @project = Factory.create(:project) }
     
-    describe "of system generated fields" do
-      it "should automatically create name field based on display name input" do
-        @project = Factory.build(:project)
-        @project.valid?.should == true
-        @project.name.should == "build-death-star"
-      end
-      
-      it "should escape special characters from the input" do
-        @project = Factory.build(:project, :display_name => "This Isn't A Valid Name!?!?!")
-        @project.valid?.should == true
-        @project.name.should == "this-isnt-a-valid-name"
-      end
-    end  
+    subject { @project }
+    it { should belong_to(:group) }
+    it { should have_many(:comments) }
+    
+    it { should validate_presence_of(:display_name) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+    it { should validate_presence_of(:focus) }
+    it { should validate_presence_of(:goal) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:audience) }
+    it { should validate_presence_of(:involves) }
+  
+    it { should validate_uniqueness_of(:name) }
   end
   
+  describe "#escape_name" do
+    before { @project = Factory.create(:project) }
+    
+    it "should generate a name field" do
+      @project.name.should_not be_nil
+    end
+    
+    it "should escape special characters" do
+      @project.display_name = "It's A Trap!?!?!"
+      @project.valid?.should == true
+      @project.name.should == "its-a-trap"    
+    end
+  end 
 end
