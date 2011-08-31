37ba91cc517c843436fb4711c89bca5e6e5c0d55
Cody Stoltman <particlebanana@gmail.com>
2011-08-31 09:03:28 -0500

    add after_create callback to sync group counter

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,7 @@ class Report
   include Mongoid::Document
   include Mongoid::Timestamps
   
+  field :group_id, type: Integer
   field :project_id, type: Integer
   field :number_of_youth_reached, type: Integer
   field :number_of_adults_reached, type: Integer
@@ -20,9 +21,10 @@ class Report
   
   attr_protected :level_of_impact
 
-  validates_presence_of [:project_id, :number_of_youth_reached, :number_of_adults_reached, :percent_male, :percent_female, :percent_african_american, :percent_asian, :percent_caucasian, :percent_hispanic, :percent_other, :money_spent, :prep_time]
-    
-  public
+  validates_presence_of [:group_id, :project_id, :number_of_youth_reached, :number_of_adults_reached, :percent_male, :percent_female, :percent_african_american, :percent_asian, :percent_caucasian, :percent_hispanic, :percent_other, :money_spent, :prep_time]
+  
+  after_create :update_counter
+      
   def filters
     money = ['', '$0-$25', '$25-$50', '$50-$100', '$100+']
     prep = ['', '1 day or less', '2 days - 1 week', '2-3 weeks', '1 month+']
@@ -32,6 +34,17 @@ class Report
     }
   end
   
+  private
+
+  def update_counter
+    counter = Counter.where(group_id: self.group_id).first
+    if counter
+      counter.update_totals({youth_total: self.number_of_youth_reached, adult_total: self.number_of_adults_reached})
+    else
+      counter = Counter.new({youth_total: self.number_of_youth_reached, adult_total: self.number_of_adults_reached})
+      counter.group_id = self.group_id
+      counter.save
     end
+  end
     
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -2,33 +2,37 @@ require 'spec_helper'
 
 describe Report do
   
-  describe "validations" do
-    
-    describe "of required fields" do     
-
-      it "should allow the creation of a report" do
-        @report = Factory.build(:report)
-        @report.should be_valid
-      end
-      
-      it "should enforce required fields" do
-        subject { Factory(:user) }
-    
-        should_not allow_value("").for(:project_id) 
-        should_not allow_value("").for(:number_of_youth_reached) 
-        should_not allow_value("").for(:number_of_adults_reached) 
-        should_not allow_value("").for(:percent_male) 
-        should_not allow_value("").for(:percent_female)
-        should_not allow_value("").for(:percent_african_american)
-        should_not allow_value("").for(:percent_asian)
-        should_not allow_value("").for(:percent_caucasian)
-        should_not allow_value("").for(:percent_hispanic)
-        should_not allow_value("").for(:percent_other)
-        should_not allow_value("").for(:money_spent)
-        should_not allow_value("").for(:prep_time)
-      end
+  it { should validate_presence_of(:group_id) }
+  it { should validate_presence_of(:project_id) }
+  
+  it { should validate_presence_of(:number_of_youth_reached) }
+  it { should validate_presence_of(:number_of_adults_reached) }
+  it { should validate_presence_of(:percent_male) }
+  it { should validate_presence_of(:percent_female) }
+  it { should validate_presence_of(:percent_african_american) }
+  it { should validate_presence_of(:percent_asian) }
+  it { should validate_presence_of(:percent_caucasian) }
+  it { should validate_presence_of(:percent_hispanic) }
+  it { should validate_presence_of(:percent_other) }
+  it { should validate_presence_of(:money_spent) }
+  it { should validate_presence_of(:prep_time) }
+  
+  describe "factory" do
+    it "should generate a report" do
+      report = Factory.create(:report)
+      report.valid? should be_true
     end
-        
   end
-
+  
+  describe "create a report" do
+    before do
+      @report = Factory.create(:report)
+    end 
+    
+    it "should update the group's counters" do
+      counter = Counter.where(group_id: @report.group_id).first
+      counter.youth_total.should == 10
+      counter.adult_total.should == 10
+    end
+  end
 end
