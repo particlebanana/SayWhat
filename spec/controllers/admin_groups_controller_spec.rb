5093358a8609eae8aedcab6af27122b7f6531a50
Cody Stoltman <particlebanana@gmail.com>
2011-09-01 13:50:33 -0500

    use Factories more efficiently

--- spec/controllers/admin_groups_controller_spec.rb
+++ spec/controllers/admin_groups_controller_spec.rb
@@ -2,50 +2,59 @@ require 'spec_helper'
 include CarrierWave::Test::Matchers
 
 describe AdminGroupsController do
-
+  before(:each) do
+    admin = Factory.create(:user, {email: "admin@test.com", role: "admin"})
+    sign_in admin
+  end
+  
   describe "#index" do
     before do
-      @group = Factory.create(:group)
-      login_admin
+      Factory.create(:group)
+      get :index
     end
     
-    it "should list all groups" do
-      get :index
+    it "should return an array of Group objects" do
       assigns[:groups].count.should == 1
-      assigns[:groups].first.display_name.should == @group.display_name
-      assigns[:groups].first.organization.should == @group.organization
+      assigns[:groups].is_a? Array
+    end
+    
+    it "should render the index template" do
+      response.should render_template('groups/index')      
     end
   end
  
   describe "#show" do
     before do
-      build_group_with_admin
-      login_admin
+      group = Factory.create(:group)
+      Factory.create(:user, {group: group, role: 'adult sponsor'})
+      get :show, id: group.id
     end
     
-    it "should find the group by id" do
-      get :show, :id => @group.id
-      assigns[:group].should_not be_nil
-      assigns[:group].display_name.should == @group.display_name
-      assigns[:group].organization.should == @group.organization
+    it "should return a Group object" do
+      assigns[:group].is_a? Group
+    end
+    
+    it "should assign @sponsor" do
+      assigns[:sponsor].is_a? User
+    end
+    
+    it "should render the show template" do
+      response.should render_template('groups/show')
     end
   end
    
   describe "#update" do
     before do
-      build_group_with_admin
-      login_admin
+      @group = Factory.create(:group)
+      Factory.create(:user, {group: @group, role: 'adult sponsor'})
     end
     
-    context "successfully" do
-      before(:each) { do_update_group() }
+    context "with valid input" do
+      before { put :update, {id: @group.id, group: { display_name: "Rebel Alliance" }} }
       
       subject{ @group.reload }
       its(:display_name) { should == "Rebel Alliance" }
       its(:name) { should == "rebel alliance" }
-    end
-  end
-  
     
       it "should redirect to #index" do
         response.should redirect_to('/admin/groups')
@@ -104,25 +113,6 @@ describe AdminGroupsController do
       it "should have an error message" do
         flash[:alert].should =~ /error removing group/i
       end
-    end
-  end
-  
-  describe "#resend" do
-    before do
-      @group = Factory.create(:pending_group, :status => "setup")
-      @user = Factory.build(:user)
-      set_status_and_role("setup", "admin")
-      @user.group = @group
-      @user.save!
-      login_admin
-    end
-    
-    context "approval email" do
-      before(:each) { do_resend }
-    
-      subject{ ActionMailer::Base.deliveries.last }
-      its(:subject) { should == "Your group has been approved on SayWhat!" }
-      its(:to) { should == ["han.solo@gmail.com"] }
     end
   end
 end
