d484cb7cfa138d2ecd3646656ff6dcc4781d82b3
Cody Stoltman <particlebanana@gmail.com>
2011-09-06 10:43:11 -0500

    refactor admin_grants controller

--- app/controllers/admin_grants_controller.rb
+++ app/controllers/admin_grants_controller.rb
@@ -2,8 +2,8 @@ class AdminGrantsController < ApplicationController
   layout "admin"
   
   before_filter :authenticate_user!
-  
-  authorize_resource :class => false
+  before_filter :set_grant, except: :index
+  authorize_resource class: false
   
   respond_to :html
   
@@ -12,29 +12,28 @@ class AdminGrantsController < ApplicationController
     @grants = Grant.approved.order('created_at DESC')
     respond_with(@grants)
   end
-  
+ 
   # GET - View A Single Rewarded Grant
   def show
-    @grant = Grant.find(params[:id])
     respond_with(@grant)
   end
     
   # DELETE - Deny a Mini-Grant
   def destroy
-    if params[:reason] && params[:reason] != "" # require a reason
-      @grant = Grant.find(params[:id])
-      reasons = YAML.load(File.read(Rails.root.to_s + "/config/denied_reasons.yml"))['reasons']['grants']
-      reason = reasons.select{|r| r['name'] == params[:reason]}[0]
-      if @grant.destroy
-        UserMailer.send_grant_denied(@grant, reason['email_text']).deliver
-        redirect_to "/admin/grants/pending", :notice => "Grant application has been removed" 
-      else
-        flash[:error] = "Error removing grant application from the system"
-        redirect_to "/admin/grants/#{@grant.id.to_s}/edit"
-      end
+    # require a reason
+    return redirect_to "/admin/grants/#{@grant.id.to_s}", alert: "Error denying grant" unless params[:reason] && params[:reason].length > 0
+    reasons = YAML.load(File.read(Rails.root.to_s + "/config/denied_reasons.yml"))['reasons']['grants']
+    reason = reasons.select{|r| r['name'] == params[:reason]}[0]
+    if @grant.deny(reason)
+      redirect_to "/admin/grants/pending", notice: "Grant application has been removed" 
     else
-      flash[:error] = "Error removing grant application from the system"
-      redirect_to "/admin/grants/#{params[:id]}/edit"
+      redirect_to "/admin/grants/#{@grant.id.to_s}/edit", alert: "Error removing grant application from the system"
     end
+  end
+  
+  private
+  
+  def set_grant
+    @grant = Grant.find(params[:id])
   end
 end
--- spec/controllers/admin_grants_controller_spec.rb
+++ spec/controllers/admin_grants_controller_spec.rb
@@ -2,59 +2,71 @@ require 'spec_helper'
 include CarrierWave::Test::Matchers
 
 describe AdminGrantsController do
-  
+  before(:each) do
+    admin = Factory.create(:user, {email: "admin@test.com", role: "admin"})
+    sign_in admin
+  end
+    
   describe "#index" do
     before do
-      @grant = Factory.create(:minigrant, :status => true)
-      login_admin
+      Factory.create(:grant, {status: true})
+      get :index
     end
     
-    it "should list all the active grants" do
-      get :index
-      assigns[:grants].first.group_name.should == @grant.group_name
-      assigns[:grants].first.adult_name.should == @grant.adult_name
+    it "should return an array of Grant objects" do
+      assigns[:grants].count.should == 1
+      assigns[:grants].is_a? Array
+    end
+    
+    it "should render the index template" do
+      response.should render_template('admin_grants/index')      
     end
   end
   
   describe "#show" do
     before do
-      @grant = Factory.create(:minigrant, :status => true)
-      login_admin
+      grant = Factory.create(:grant)
+      get :show, id: grant.id
     end
     
-    it "should find the grant by id" do
-      get :show, :id => @grant.id
-      assigns[:grant].should_not be_nil
-      assigns[:grant].group_name.should == @grant.group_name
-      assigns[:grant].adult_name.should == @grant.adult_name
+    it "should return a Grant object" do
+      assigns[:grant].is_a? Grant
+    end
+    
+    it "should render the show template" do
+      response.should render_template('admin_grants/show')
     end
   end
-   
+  
   describe "#destroy" do
-    before(:each) do
-      @grant = Factory.create(:minigrant, :status => false)
-      login_admin
-    end
-
-    context "successfully" do
-      before(:each) { do_destroy_grant(id: "#{@grant.id.to_s}", reason: "curriculum")}
-
-      subject{ response }
-      it { should redirect_to('/admin/grants/pending') }
-      it "should destroy the application" do
-        Grant.where(_id: "#{@grant.id}").first.should == nil
+    before { @grant = Factory.create(:grant) }
+    
+    context "given a reason" do
+      before { post :destroy, {id: @grant.id.to_s, reason: "curriculum"} }
+      
+      it "should remove the grant from the database" do
+        Grant.where(id: @grant.id).first.should == nil
       end
-
-      subject{ ActionMailer::Base.deliveries.last }
-      its(:subject) { should == "SayWhat! Mini-Grant Has Been Denied" }
-      its(:to) { should == ["han.solo@gmail.com"] }  
+      
+      it "should have a success message" do
+        flash[:notice].should =~ /application has been removed/i
+      end
+      
+      it "should send the an email" do
+        ActionMailer::Base.deliveries.last.subject.should =~ /grant has been denied/i
+      end
     end
-
-    context "un-successfully" do
-      before(:each) { do_destroy_grant(id: "#{@grant.id.to_s}", reason: "") }
-
-      subject { response }
-      it { should redirect_to("/admin/grants/#{@grant.id}/edit") }
+    
+    context "without reason" do
+      before { post :destroy, {id: @grant.id.to_s} }
+      
+      it "should redirect to requests" do
+        response.should redirect_to("/admin/grants/#{@grant.id}")
+      end
+      
+      it "should have an error message" do
+        flash[:alert].should =~ /error denying grant/i
+      end
     end
   end
 end
