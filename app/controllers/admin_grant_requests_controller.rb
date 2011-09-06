fb2abfcb9cf0f6fea1d1adc374e39c061a9c96c4
Cody Stoltman <particlebanana@gmail.com>
2011-09-06 11:29:46 -0500

    refactor admin_grant_requests controller

--- app/controllers/admin_grant_requests_controller.rb
+++ app/controllers/admin_grant_requests_controller.rb
@@ -2,8 +2,8 @@ class AdminGrantRequestsController < ApplicationController
   layout "admin"
   
   before_filter :authenticate_user!
-  
-  authorize_resource :class => false
+  before_filter :set_grant, except: :index
+  authorize_resource class: false
   
   respond_to :html
   
@@ -15,26 +15,27 @@ class AdminGrantRequestsController < ApplicationController
   
   # GET - Edit A Grant Application
   def edit
-    @grant = Grant.find(params[:id])
     respond_with(@grant)
   end
   
   # PUT - Approve a Grant Application
   def update
-    @grant = Grant.find(params[:id])
-    @grant.status = true
-    if @grant.save
-      UserMailer.send_grant_approval(@grant).deliver
-      redirect_to "/admin/grants"
+    if @grant.approve
+      redirect_to "/admin/grants", notice: "Grant was approved"
     else
-      redirect_to "/admin/grants/#{@grant.id.to_s}/edit", :notice => "Error saving record"
+      redirect_to "/admin/grants/#{@grant.id.to_s}/edit", alert: "Error saving record"
     end
   end
-  
+ 
   # Load grant denied reasons from YAML file and return as json
   def destroy
     @reasons = YAML.load(File.read(Rails.root.to_s + "/config/denied_reasons.yml"))['reasons']['grants']
     render :layout => false
   end
+
+  private
   
+  def set_grant
+    @grant = Grant.find(params[:id])
+  end
 end
--- spec/controllers/admin_grant_requests_controller_spec.rb
+++ spec/controllers/admin_grant_requests_controller_spec.rb
@@ -2,51 +2,73 @@ require 'spec_helper'
 include CarrierWave::Test::Matchers
 
 describe AdminGrantRequestsController do
-
+  before(:each) do
+    admin = Factory.create(:user, {email: "admin@test.com", role: "admin"})
+    sign_in admin
+  end
+  
   describe "#index" do
     before do
-      @grant = Factory.create(:minigrant, :status => false)
-      login_admin
+      Factory.create(:grant)
+      get :index
     end
     
-    it "should list all the pending grants" do
-      get :index
+    it "should return an array of Grant objects" do
       assigns[:grants].count.should == 1
-      assigns[:grants].first.group_name.should == @grant.group_name
+      assigns[:grants].is_a? Array
+    end
+    
+    it "should render the index template" do
+      response.should render_template('admin_grant_requests/index')      
     end
   end
   
   describe "#edit" do
     before do
-      @grant = Factory.create(:minigrant, :status => false)
-      login_admin
+      grant = Factory.create(:grant)
+      get :edit, id: grant.id.to_s
     end
     
-    it "should find the pending grant by id" do
-      get :edit, :id => @grant.id
-      assigns[:grant].group_name.should == @grant.group_name
-      assigns[:grant].adult_name.should == @grant.adult_name
+    it "should return a Grant object" do
+      assigns[:grant].is_a? Grant
+    end
+    
+    it "should render the edit template" do
+      response.should render_template('admin_grant_requests/edit')
     end
   end
- 
+  
   describe "#update" do
-    before(:each) do
-      @grant = Factory.create(:minigrant, :status => false)
-      login_admin
+    before do
+      @grant = Factory.create(:grant)
+      put :update, id: @grant.id.to_s
     end
     
-    context "successfully" do
-      before(:each) { do_update_grant(id: "#{@grant.id}") }
-      
-      subject { response }
-      it { should redirect_to('/admin/grants') }
-            
-      it "should send the adult contact an email" do
-        ActionMailer::Base.deliveries.last.subject.should == "SayWhat! Mini-Grant Has Been Approved"
-      end
-            
-      subject{ @grant.reload }
-      its(:status) { should == true }
+    it "should set the grant status to true" do
+      @grant.reload.status.should == true
+    end
+    
+    it "should redirect to grants index" do
+      response.should redirect_to('/admin/grants')
+    end
+    
+    it "should send an email" do
+      ActionMailer::Base.deliveries.last.subject.should =~ /grant has been approved/i
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      grant = Factory.create(:grant)
+      get :destroy, id: grant.id.to_s
+    end
+    
+    it "should assign @reasons" do
+      assigns[:reasons].size.should > 0
+    end
+    
+    it "should not render a layout" do
+      response.should_not render_template('layouts/admin')
     end
   end
 end
