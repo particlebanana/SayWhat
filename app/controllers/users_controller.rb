2ed24f2cfc42dd0f8415492d8c1638e9d081a6e8
Cody Stoltman <particlebanana@gmail.com>
2011-09-12 11:22:17 -0500

    cleanup old methods from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,81 +2,28 @@ class UsersController < ApplicationController
   layout "application"
   
   before_filter :authenticate_user!
+  before_filter :set_user
   load_and_authorize_resource
   
   respond_to :html
-  
-  ###############################################
-  # Edit user settings (profile, password, etc)
-  ###############################################
-  
-  # GET - Edit Basic Profile Information
+    
+  # GET - Edit User
   def edit
-    @user = current_user
     respond_with(@user)
   end
   
-  # PUT - Update Basic Profile Information
+  # PUT - Update User
   def update
     if @user.update_attributes(params[:user])
-      redirect_to "/settings/profile", :notice => "Profile has been updated"
+      redirect_to "/settings/profile", notice: "Profile has been updated"
     else
-      render :action => "edit"
+      render action: "edit", alert: "Error updating profile. Try again."
     end
   end
+
+  private
   
-  # GET - Delete Avatar
-  def delete_avatar
-    @user.remove_avatar!
-    @user.avatar_filename = nil
-    @user.save
-    redirect_to "/settings/profile"
-  end
-  
-  # GET - Edit User Password
-  def edit_password
+  def set_user
     @user = current_user
-    respond_with(@user)
   end
-  
-  # PUT - Update User Password
-  def update_password
-    if @user.update_attributes(params[:user])
-      redirect_to "/settings/password", :notice => "Password has been updated"
-    else
-      render :action => "edit_password"
-    end
-  end
-  
-  ###############################################
-  # Approve Group Membership Request
-  ###############################################
-  
-  #
-  # Only a group sponsor can approve a potential member for
-  # inclusion into a group, even if that member was invited.
-  #
-    
-  # GET - Approve Pending Group Member
-  def approve_pending_membership
-    @user.status = "active"
-    @user.role = "member"
-    if @user.save
-      message = current_user.messages.find(params[:message])
-      message.delete
-      UserMailer.send_approved_notice(@user, @user.group, request.env["HTTP_HOST"]).deliver
-      redirect_to "/messages", :notice => "Member has been added"
-    else
-      redirect_to "/messages", :notice => "There was an error approving user"
-    end
-  end
-  
-  # GET - Deny Pending Group Member
-  def deny_pending_membership
-    message = current_user.messages.find(params[:message])
-    message.delete
-    @user.delete
-    redirect_to "/messages", :notice => "Member has been removed"
-  end
-    
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,106 +1,40 @@
 require 'spec_helper'
 
 describe UsersController do
-      
-  describe "edit a user's profile" do
-    before(:each) do
-      @user = Factory.build(:user)
-      set_status_and_role("active", "adult sponsor")
-      @user.save
-      sign_in @user
-    end
-    
-    describe "#update" do
-      it "should change the user's name" do
-        put :update, {:id => @user.id.to_s, :user => {:first_name => "Jabba", :last_name => "The Hut"}}
-        @user.reload.name.should == "Jabba The Hut"
-      end
-    
-      it "should change the user's email" do
-        put :update, {:id => @user.id.to_s, :user => {:email => "test@example.com"}}
-        @user.reload.email.should == "test@example.com"
-      end
-    
-      it "should populate the bio field" do
-        put :update, {:id => @user.id.to_s, :user => {:bio => "This is an example bio"}}
-        @user.reload.bio.should_not == nil
-      end
-    end
-    
-  end
   
-  describe "approve a pending membership request" do
-    before(:each) do
-      build_group_with_admin
-      @user = Factory.build(:user, :email => "billy.bob@gmail.com")
-      set_status_and_role("pending", "pending")
-      @group.users << @user
-      @user.save
-      @group.save
-      @message = @admin.create_message_request_object(@user.name, @user.email, @user.id.to_s)
-      sign_in @admin
-    end
-    
-    describe "#approve_pending_membership" do
-      it "should change the users status to active" do
-        put :approve_pending_membership, {:id => @user.id.to_s, :message => @message.id.to_s}
-        @user.reload.status.should == "active"
-      end
-    
-      it "should change the users role to member" do
-        put :approve_pending_membership, {:id => @user.id.to_s, :message => @message.id.to_s}
-        @user.reload.role.should == "member"
-      end
-    
-      it "should send the new member an email" do
-        put :approve_pending_membership, {:id => @user.id.to_s, :message => @message.id.to_s}
-        ActionMailer::Base.deliveries.last.to.should == [@user.email]
-      end
-    end
-    
-  end
-  
-  describe "manage a group youth sponsor" do
+  describe "#edit" do
     before do
-      build_group_with_admin
-      sign_in @admin
+      user = Factory.create(:user)
+      sign_in user
+      get :edit
     end
     
-    describe "#choose_youth_sponsor" do
-      it "should list all the active members who are not sponsors" do
-        get :choose_youth_sponsor, :permalink => @group.permalink
-        assigns[:members].count.should == 1
-      end
+    it "should assign @user" do
+      assigns[:user].is_a? User
     end
     
-    describe "#assign_youth_sponsor" do
-      it "should change the users status to youth sponsor" do
-        @user.role.should == "member"
-        put :assign_youth_sponsor, :permalink => @group.permalink, :user_id => @user.id.to_s
-        response.should be_redirect
-        @user.reload.role.should == "youth sponsor"
-      end
-      
-      it "should send the member an email notification" do
-        put :assign_youth_sponsor, :permalink => @group.permalink, :user_id => @user.id.to_s
-        ActionMailer::Base.deliveries.last.to.should == [@user.email]
-      end
-    end
-    
-    describe "#revoke_youth_sponsor" do
-      it "should change the users status to member" do
-        @user.update_attributes!(:role => "youth sponsor")
-        put :revoke_youth_sponsor, :permalink => @group.permalink, :user_id => @user.id.to_s
-        response.should be_redirect
-        @user.reload.role.should == "member"
-      end
-      
-      it "should send the member an email notification" do
-        @user.update_attributes!(:email => "revoked@gmail.com", :role => "member")
-        put :revoke_youth_sponsor, :permalink => @group.permalink, :user_id => @user.id.to_s
-        ActionMailer::Base.deliveries.last.to.should == [@user.email]
-      end
+    it "should render edit template" do
+      response.should render_template('users/edit')
     end
   end
-
+  
+  describe "#update" do
+    before do
+      @user = Factory.create(:user)
+      sign_in @user
+      put :update, {:id => @user.id, :user => {first_name: "Jabba", last_name: "The Hut"}}
+    end
+    
+    it "should update the User object" do
+      @user.reload.name.should == "Jabba The Hut"
+    end
+    
+    it "should redirect to edit action" do
+      response.should redirect_to('/settings/profile')
+    end
+    
+    it "should set a notice message" do
+      flash[:notice].should =~ /has been updated/i
+    end
+  end
 end
