dba708098f61a10b36fb789154310a97c26fbf66
Cody Stoltman <particlebanana@gmail.com>
2011-09-19 11:38:49 -0500

    automatically parse urls into links on save

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class Announcement
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -6,4 +7,13 @@ class Announcement
   field :text
 
   validates_presence_of :title, :text
+
+  before_save :clean_string
+
+  protected
+
+  def clean_string
+    self.text.gsub!(/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/) { |x| "<a href='#{x}'>#{x}</a>"}
+  end
+
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -3,7 +3,20 @@ require 'spec_helper'
 describe Announcement do
   before { @announcement = Announcement.create( { title: "Test", text: "Test Announcement" } ) }
   
-  subject { @announcement }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:text) }
+  describe "Validations" do
+    subject { @announcement }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:text) }
+  end
+
+  describe "#clean_string" do
+    before do
+      @announcement.text = "This text contains one http://www.test.com/link link."
+      @announcement.save
+    end
+
+    it "should turn urls in the text into links" do
+      @announcement.text.should == "This text contains one <a href='http://www.test.com/link'>http://www.test.com/link</a> link."
+    end
+  end
 end
