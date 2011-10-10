2b2b2a8664b7c8eb5691bc908a11337d877826e0
Cody Stoltman <particlebanana@gmail.com>
2011-10-10 11:14:18 -0500

    move the comment object building into the model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ require "time"
 
 class Comment < Hashie::Dash
 
-  property :url, default: nil
   property :comment
   property :parent, default: nil
   property :timelines, default: []
@@ -13,6 +12,52 @@ class Comment < Hashie::Dash
     super(*args)
   end
 
+  # Initializes a new Comment object and adds attributes
+  # based on which type of comment is being created.
+  # All comment types must include the comment text and user object.
+  #
+  # Comment types include:
+  #
+  #  Group: A comment on a group's timeline
+  #     - Requires Group object to not be nil
+  #     ex: Comment.build("text", User, Group)
+  #
+  #  Project: A comment on a group's project timeline
+  #     - Requires Group and Project objects to not be nil
+  #     ex: Comment.build("text", User, Group, Project)
+  #
+  #  Nested - A comment response to another comment
+  #     - Requires parent param to not be nil and Project and Group to be nil
+  #     ex: Comment.build("text", User, nil, nil, "Comment:1234")
+  #
+  def self.build(text, user, group=nil, project=nil, parent=nil)
+    comment = Comment.new({
+      comment: text,
+      objects: { user: "user:#{user.id}" },
+    })
+
+    # Project Comment
+    if project
+      comment.objects.merge!({ group: "group:#{group.id}", project: "project:#{project.id}" })
+      comment.timelines = ["project:#{project.id}"]
+    # Nested Comment
+    elsif parent
+      comment.parent = parent
+      comment.timelines = ["#{parent}"]
+    # Group Comment
+    else
+      comment.objects.merge!({ group: "group:#{group.id}" })
+      comment.timelines = ["group:#{group.id}"]
+    end
+
+    comment
+  end
+
+  # Creates a new Chronologic::Event from the comment object.
+  # The $feed.publish method returns the new Event object's URL.
+  # Save method should do a find in order to return the complete event.
+  # This allows the event to be created asynchronously and updated on the
+  # timeline if necessary.
   def save
     return false if comment.nil?
     event = Chronologic::Event.new(
@@ -24,6 +69,7 @@ class Comment < Hashie::Dash
 
     event.data.merge!({ parent: self.parent }) unless self.parent.nil?
 
-    $feed.publish(event, true, Time.now.utc.tv_sec)
+    url = $feed.publish(event, true, Time.now.utc.tv_sec)
+    HTTParty.get(url).body
   end
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe Comment do
+  before do
+    @user = Factory.create(:user)
+    @group = Factory.create(:group)
+    @project = Factory.create(:project, { group: @group } )
+  end
+
+  describe ".build" do
+    context "group timeline" do
+      before { @comment = Comment.build("test", @user, @group) }
+
+      it "should return a comment object" do
+        (@comment.is_a? Comment).should be_true
+      end
+
+      it "should create a hash with 2 comment objects" do
+        (@comment['objects'].is_a? Hash).should be_true
+        @comment['objects'].count.should == 2
+      end
+
+      it "should create an array of timelines" do
+        (@comment['timelines'].is_a? Array).should be_true
+        @comment['timelines'].count.should == 1
+        @comment['timelines'].should == ["group:#{@group.id}"]
+      end
+    end
+
+    context "project timeline" do
+      before { @comment = Comment.build("test", @user, @group, @project) }
+
+      it "should return a comment object" do
+        (@comment.is_a? Comment).should be_true
+      end
+
+      it "should create a hash with 3 comment objects" do
+        (@comment['objects'].is_a? Hash).should be_true
+        @comment['objects'].count.should == 3
+      end
+
+      it "should create an array of timelines with a length of 1" do
+        (@comment['timelines'].is_a? Array).should be_true
+        @comment['timelines'].count.should == 1
+        @comment['timelines'].should == ["project:#{@project.id}"]
+      end
+    end
+
+    context "nested comment" do
+      before do
+        obj = Comment.build("test", @user, @group, @project)
+        @parent = JSON(obj.save)
+        sleep(1)
+        @comment = Comment.build("test2", @user, nil, nil, @parent['event']['key'])
+      end
+
+      it "should return a comment object" do
+        (@comment.is_a? Comment).should be_true
+      end
+
+      it "should set the parent attribute" do
+        @comment.parent.should == @parent['event']['key']
+      end
+
+      it "should create an array of timelines with a length of 1" do
+        (@comment['timelines'].is_a? Array).should be_true
+        @comment['timelines'].count.should == 1
+        @comment['timelines'].should == ["#{@parent['event']['key']}"]
+      end
+    end
+  end
+
+  describe "#save" do
+    before do
+      comment_obj = Comment.build("test", @user, @group)
+      @comment = JSON(comment_obj.save)
+    end
+
+    it "should create a new Chronologic::Event" do
+      @comment['event']['data']['comment'].should == 'test'
+    end
+  end
+end
