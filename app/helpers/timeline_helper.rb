fa3db22d4ed8740ef36b818c44199014c79d8b4f
Cody Stoltman <particlebanana@gmail.com>
2011-10-13 15:23:13 -0500

    make timeline work with group project comments

--- app/helpers/timeline_helper.rb
+++ app/helpers/timeline_helper.rb
@@ -1,8 +1,8 @@
 module TimelineHelper
 
-  def build_event(event)
+  def build_event(event, section_key)
     return unless event.data.type
-    type, obj = event.data.type, EventTimeline.new(event)
+    type, obj = event.data.type, EventTimeline.new(event, section_key)
     case type
     when "message"
       event = obj.build_message
@@ -10,8 +10,10 @@ module TimelineHelper
       objects = event.objects
       if objects.project
         form_url = group_project_comment_comments_path(objects.group.id, objects.project.id, event['key'])
+        event_section = 'project'
       elsif objects.group
         form_url = group_comment_comments_path(objects.group.id, event['key'])
+        event_section = 'group'
       end
 
       render 'timeline/comment', {
@@ -19,6 +21,7 @@ module TimelineHelper
         form: form_url,
         user: event.objects.user,
         objects: event.objects,
+        title: obj.build_title(event_section),
         comment: event.data.comment,
         timestamp: Time.at(event.token.split(':')[1].to_i),
         subevents: event.subevents
@@ -28,9 +31,10 @@ module TimelineHelper
 
   class EventTimeline
 
-    def initialize(event)
+    def initialize(event, section_key)
       @data = event.data
       @objects = event.objects
+      @section_key = section_key
     end
 
     # Build a message event type in the timeline.
@@ -40,9 +44,33 @@ module TimelineHelper
       html = ""
       @objects.to_a.each do |obj|
         klass = obj[0]
-        msg.gsub!(obj[1].name, "<a href='/#{klass.pluralize}/#{obj[1].id}'>#{obj[1].name}</a>")
+        case klass
+        when 'group'
+          url = "/#{klass.pluralize}/#{obj[1].id}"
+        when 'project'
+          url = "/groups/#{@objects.group.id}/#{klass.pluralize}/#{obj[1].id}"
+        end
+        msg.gsub!(obj[1].name, "<a href='#{url}'>#{obj[1].name}</a>")
       end
       html << "<p>" + msg + "</p>"
+    end
+
+    # Build an event title for use in timeline
+    # EX: <h6>
+    def build_title(section)
+      html = ""
+      case @data.type
+      when 'comment'
+        title = "<a href='/users/#{@objects.user.id}'>#{@objects.user.name}</a> "
+        if (@objects.include? 'project') && (section != @section_key)
+          title << "<span class='arrow'></span> "
+          title << "<a href='/groups/#{@objects.group.id}/projects/#{@objects.project.id}'>#{@objects.project.name}</a>"
+        elsif (@objects.include? 'group') && (section != @section_key)
+          title << "<span class='arrow'></span> "
+          title << "<a href='/groups/#{@objects.group.id}'>#{@objects.group.name}</a>"
+        end
+      end
+      html << "<h6 class='cf'>#{title}</h6>"
     end
 
     private
