6aff312d503850922362b3e8c3ddb2381b5a6335
Cody Stoltman <particlebanana@gmail.com>
2011-10-18 17:19:05 -0500

    update post-processing resize on profile photos

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -3,38 +3,28 @@ require 'carrierwave/processing/mini_magick'
 class ProfileUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  storage :file
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "profile/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-  
   def default_url
     "/default/fallback/" + [version_name, "profile.jpg"].compact.join('_')
   end
   
-  process :resize_to_fit => [800, 800]
+  process resize_to_limit: [800, 800]
   
   version :thumb do
-    process :resize_to_fill => [50, 50]
-  end
-  
-  version :small do
-    process :resize_to_fill => [200, 100]
+    process resize_to_fill: [50, 50]
   end
   
   version :profile do
-    process :resize_to_fit => [250, 250]
-  end
-  
-  version :large do
-    process :resize_to_fit => [278, 278]
-  end
+    process resize_to_limit: [270, 270]
+end
 
   # Add a white list of extensions which are allowed to be uploaded.
    def extension_white_list
