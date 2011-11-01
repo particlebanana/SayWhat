1326b886c145e0aad21c4fe89200834566ee1ead
Cody Stoltman <particlebanana@gmail.com>
2011-11-01 15:39:56 -0500

    cleanup config and add mongo_config global

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,1 +1,2 @@
+Mongo_Config = YAML.load_file(File.join(File.dirname(__FILE__), '../mongodb_config.yml'))
 $mongo = Mongo::Connection.new("localhost", 27017, logger: Rails.logger).db("saywhat_#{Rails.env.downcase}")
