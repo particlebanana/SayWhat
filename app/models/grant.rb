cdb440b0d6d1a4d3f41c768d8e4e6c326bc9dcf6
Cody Stoltman <particlebanana@gmail.com>
2011-11-02 10:12:25 -0500

    approve grant method

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -6,18 +6,56 @@ class Grant < ActiveRecord::Base
 
   validates_presence_of [:status, :member, :planning_team, :serviced, :goals, :funds_use, :partnerships, :resources]
 
-  def notify_sponsor(host, requestor)
-    GrantMailer.finalization_notification(host, requestor, self.project.group, self.project, self).deliver
-    send_notification
+  # Scopes
+  def self.completed
+    where(status: "completed")
+  end
+
+  def self.approved
+    where(status: "approved")
+  end
+
+  def notify_sponsor_of_application(host, requestor)
+    GrantMailer.finalization_notification(host, requestor, self.project.group, self.project, self).deliver 
+    message = {
+      text: I18n.t('notifications.grant.new_application'),
+      link: "/groups/#{self.project.group.permalink}/projects/#{self.project.id}/grants/#{self.id}/edit"
+    }
+    create_notification(message)
+  end
+
+  def approve
+    self.status = 'approved'
+    if self.save
+      GrantMailer.grant_approved(self).deliver
+      messages = build_approved_messages
+      create_notification(messages[:notification])
+      self.project.publish_to_feed(messages[:event][:key], messages[:event][:text])
+      true
+    else
+      false
+    end
   end
 
   private
 
-  def send_notification
+  def create_notification(message)
     sponsor = self.project.group.adult_sponsor
     notification = Notification.new(sponsor.id)
-    text = I18n.t 'notifications.grant.new_application'
-    link = "/groups/#{self.project.group.permalink}/projects/#{self.project.id}/grants/#{self.id}/edit"
-    notification.insert(text, link)
+    notification.insert(message)
+  end
+
+  # Builds the text, links, NoSQL keys, etc for an approved grant
+  def build_approved_messages
+    messages = {
+      notification: {
+        text: I18n.t('notifications.grant.approved'),
+        link: "/groups/#{self.project.group.permalink}/projects/#{self.project.id}"
+      },
+      event: {
+        key: "project:#{self.project.id}:grant:#{self.id}:approved",
+        text: "#{self.project.display_name} has been awarded a mini-grant!"
+      }
+    }
   end
 end
--- spec/models/grant_spec.rb
+++ spec/models/grant_spec.rb
@@ -22,9 +22,9 @@ describe Grant do
     it { should validate_presence_of(:resources) }
   end
 
-  describe "notify_sponsor" do
+  describe "notify_sponsor_of_application" do
     before do
-      @grant.notify_sponsor('http://test.com', @user)
+      @grant.notify_sponsor_of_application('http://test.com', @user)
       @notifications = Notification.find(@sponsor.id)
     end
 
@@ -34,6 +34,39 @@ describe Grant do
 
     it "should create a new notification" do
       @notifications.count.should == 1
+    end
+  end
+
+  describe "approve" do
+    before do
+      @status = @grant.approve
+      @notifications = Notification.find(@sponsor.id)
+    end
+
+    it "should return true" do
+      @status.should == true
+    end
+
+    it "should set the status to approved" do
+      @grant.reload.status.should == "approved"
+    end
+
+    it "should send group sponsor an email" do
+      ActionMailer::Base.deliveries.last.to.should == [@sponsor.email]
+    end
+
+    it "should create a new notification" do
+      @notifications.count.should == 1
+    end
+
+    it "should publish to the project timeline" do
+      timeline = $feed.timeline("project:#{@project.id}")
+      timeline["feed"].first["key"].should include("project:#{@project.id}:grant:#{@grant.id}:approved")
+    end
+
+    it "should publish to the group timeline" do
+      timeline = $feed.timeline("group:#{@project.group_id}")
+      timeline["feed"].first["key"].should include("project:#{@project.id}:grant:#{@grant.id}:approved")
     end
   end
 end
