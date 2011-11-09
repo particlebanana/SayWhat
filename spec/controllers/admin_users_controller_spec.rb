e7578c16e89a394b7986a594442b9e632fae540c
Cody Stoltman <particlebanana@gmail.com>
2011-11-09 09:32:44 -0600

    cleanup unused files

--- spec/controllers/admin_users_controller_spec.rb
+++ spec/controllers/admin_users_controller_spec.rb
@@ -54,7 +54,7 @@ describe AdminUsersController do
     end
     
     context "member" do
-      before(:each) { do_destroy_user(id: @user.id) }
+      before { delete :destroy, { id: @user.id } }
       
       it "should delete" do
         User.where(id: @user.id).first.should == nil
@@ -62,26 +62,12 @@ describe AdminUsersController do
     end
     
     context "adult sponsor" do
-      before(:each) { do_destroy_user(id: @sponsor.id) }
+      before { delete :destroy, { id: @user.id } }
       
       it "should not delete" do
         User.where(id: @sponsor.id).first.should_not == nil
       end
     end
   end
-=begin 
-  describe "#remove_avatar" do
-    before(:each) do
-      setup_user_avatar
-      login_admin
-    end
-        
-    context "admin panel" do      
-      it "should completely remove an inappropriate avatar" do
-        do_remove_avatar
-        @user.reload.avatar.should == nil
-      end
-    end
-  end
-=end
+
 end
